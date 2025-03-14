20b768c375e59ac5f8f906c8a5139b23ff0d80ea
Eloy Pineda <code@eloy.email>
2025-03-14 08:11:40 +0100

    fix: add missing type to dora component

--- frontend/src/components/DoraMetrics.tsx
+++ frontend/src/components/DoraMetrics.tsx
@@ -2,13 +2,16 @@ import { Heading, Link, SimpleGrid, Text } from "@chakra-ui/react";
 import { LuExternalLink } from "react-icons/lu";
 import Metric from "./Metric";
 
-type DoraMetricsProps = {
-  data?: unknown;
-};
+interface DoraMetricsProps {
+  data?: {
+    changeLeadTime: number;
+    deploymentFrequency: number;
+    changeFailureRate: number;
+    deploymentRecoveryTime: number;
+  };
+}
 
 const DoraMetrics = ({ data }: DoraMetricsProps) => {
-  // console.log(data);
-
   return (
     <>
       <Heading mb={4}>Dora Metrics</Heading>
@@ -26,25 +29,25 @@ const DoraMetrics = ({ data }: DoraMetricsProps) => {
           label="Change Lead Time"
           helperText="Time it takes to go from code commit to code successfully running in
           production."
-          value={(data?.changeLeadTime / 86400).toFixed(2)}
+          value={((data?.changeLeadTime ?? 0) / 86400).toFixed(2)}
           unit="days"
         />
         <Metric
           label="Deployment Frequency"
           helperText="How often application changes are deployed to production."
-          value={data?.deploymentFrequency}
+          value={data?.deploymentFrequency ?? 0}
           unit="per day"
         />
         <Metric
           label="Change Failure Rate"
           helperText="Percentage of deployments causing a failure in production."
-          value={(data?.changeFailureRate * 100).toFixed(2)}
+          value={(data?.changeFailureRate ?? 0 * 100).toFixed(2)}
           unit="%"
         />
         <Metric
           label="Failed Deployment Recovery Time"
           helperText="Time it takes to recover from a failed deployment."
-          value={data?.deploymentRecoveryTime}
+          value={data?.deploymentRecoveryTime ?? 0}
           unit="hours"
         />
       </SimpleGrid>
